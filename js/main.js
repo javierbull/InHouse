07ae1125eae6da6f3f01917efdfbf8babcd12fbc
javierbull <javierbullreyes@gmail.com>
2022-08-01 12:26:02 -0500

    base selct 2 b5

--- js/main.js
+++ js/main.js
@@ -80,5 +80,9 @@ $(".showHome").click(function() {
 });
 // END SHOW HIDE PANELS
 
-
+$( '#single-select-field' ).select2( {
+    theme: "bootstrap-5",
+    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+    placeholder: $( this ).data( 'placeholder' ),
+} );
 });
